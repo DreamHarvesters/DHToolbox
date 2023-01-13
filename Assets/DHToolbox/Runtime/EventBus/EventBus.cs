ffb2c7440c9cc72ecc01c9010446aee3ea074d85
Emre SOLAK <s.emresolak@dreamharvesters.com>
2023-01-13 18:01:44 +0300

    feat: get observable from event bus

--- Assets/DHToolbox/Runtime/EventBus/EventBus.cs
+++ Assets/DHToolbox/Runtime/EventBus/EventBus.cs
@@ -14,6 +14,9 @@ namespace DHToolbox.Runtime.EventBus
         public IDisposable Subscribe<T>(IObserver<IEvent> observer) where T : IEvent =>
             eventRaised.Where(@event => @event.GetType() == typeof(T)).Subscribe(observer);
 
+        public IObservable<T> SubscribeTo<T>() where T : class, IEvent =>
+            eventRaised.Where(@event => @event.GetType() == typeof(T)).Select(@event => @event as T);
+
         public void Raise(IEvent @event) => eventRaised.OnNext(@event);
 
         public IDisposable Subscribe(IObserver<IEvent> observer) =>
