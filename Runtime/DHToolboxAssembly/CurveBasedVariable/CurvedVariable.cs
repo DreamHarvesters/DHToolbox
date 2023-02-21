b0355477eaa6cf4f079ee157a7bc14359d1463a8
Emre SOLAK <s.emresolak@dreamharvesters.com>
2023-02-21 11:47:42 +0300

    feat: log methods for curved vars

--- Runtime/DHToolboxAssembly/CurveBasedVariable/CurvedVariable.cs
+++ Runtime/DHToolboxAssembly/CurveBasedVariable/CurvedVariable.cs
@@ -1,4 +1,7 @@
 using System;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
 using UnityEngine;
 
 namespace DHToolbox.Runtime.DHToolboxAssembly.CurveBasedVariable
@@ -15,5 +18,19 @@ namespace DHToolbox.Runtime.DHToolboxAssembly.CurveBasedVariable
         public T Min => min;
 
         public abstract T Evaluate(float normalizedRate);
+
+#if ODIN_INSPECTOR
+        [Button]
+        void LogValue(float rate) => Debug.Log($"Value: {Evaluate(rate)}");
+
+        [Button]
+        void LogInterval(float from, float to, float step)
+        {
+            for (float i = from; i < to; i += step)
+            {
+                LogValue(i);
+            }
+        }
+#endif
     }
 }
