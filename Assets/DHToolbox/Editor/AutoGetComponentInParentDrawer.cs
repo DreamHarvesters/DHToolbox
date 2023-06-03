72e734a6d882e7e45150e45ec0b73a2f7aae69cf
Emre SOLAK <s.emresolak@dreamharvesters.com>
2023-06-03 21:17:23 +0300

    feat: auto get component attribute

---
+++ Assets/DHToolbox/Editor/AutoGetComponentInParentDrawer.cs
@@ -0,0 +1,30 @@
+using DHToolbox.Runtime.DHToolboxAssembly.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameAssets.Scripts.Utils
+{
+    [CustomPropertyDrawer(typeof(AutoGetComponentInParentAttribute))]
+    public class AutoGetComponentInParentDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, UnityEditor.SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            UnityEditor.EditorGUI.PropertyField(position, property, label);
+
+            if (property.objectReferenceValue == null)
+            {
+                var parentComponent = property.serializedObject.targetObject as Component;
+                if (parentComponent != null)
+                {
+                    var componentType = fieldInfo.FieldType;
+                    var component = parentComponent.GetComponentInChildren(componentType);
+                    property.objectReferenceValue = component;
+                }
+            }
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
