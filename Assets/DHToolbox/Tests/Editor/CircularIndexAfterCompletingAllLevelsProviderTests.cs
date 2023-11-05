d3637d6df961fdc1d6d8bb816f5a9f7d6e4fe7c0
Emre SOLAK <s.emresolak@dreamharvesters.com>
2023-11-05 14:13:26 +0300

    feat: level index provider that circulates after all scenes completed

---
+++ Assets/DHToolbox/Tests/Editor/CircularIndexAfterCompletingAllLevelsProviderTests.cs
@@ -0,0 +1,78 @@
+using DHToolbox.Runtime.DHToolboxAssembly.GameLevels;
+using DHToolbox.Runtime.DHToolboxAssembly.Indexing;
+using NUnit.Framework;
+using UniRx;
+
+namespace GameFoundations.Tests.Editor
+{
+    public class CircularIndexAfterCompletingAllLevelsProviderTests
+    {
+        [Test]
+        public void SequentialIndexGeneration()
+        {
+            // Arrange
+            int firstLevelIndex = 1;
+            int levelCount = 5;
+            int circulateFromLevelIndex = 3;
+            var provider = new CircularIndexAfterCompletingAllLevelsProvider(
+                firstLevelIndex, levelCount, circulateFromLevelIndex, firstLevelIndex);
+
+            // Act and Assert
+            for (int i = firstLevelIndex; i <= levelCount; i++)
+            {
+                Assert.AreEqual(i, provider.Current);
+                provider.MoveNext();
+            }
+        }
+
+        [Test]
+        public void CircularIndexGeneration()
+        {
+            // Arrange
+            int firstLevelIndex = 1;
+            int levelCount = 5;
+            int circulateFromLevelIndex = 3;
+            int currentLevel = 6; // Beyond lastLevelIndex
+            var provider = new CircularIndexAfterCompletingAllLevelsProvider(
+                firstLevelIndex, levelCount, circulateFromLevelIndex, currentLevel);
+
+            var testProvider =
+                new CircularIndexProvider(circulateFromLevelIndex, firstLevelIndex + levelCount, currentLevel);
+            // Act and Assert
+            for (int i = circulateFromLevelIndex; i <= firstLevelIndex + levelCount; i++)
+            {
+                Assert.AreEqual(testProvider.Current, provider.Current);
+                testProvider.MoveNext();
+                provider.MoveNext();
+            }
+        }
+
+        [Test]
+        public void SwitchToCircularIndexFromClampedIndex()
+        {
+            // Arrange
+            int firstLevelIndex = 1;
+            int levelCount = 5;
+            int circulateFromLevelIndex = 3;
+            int currentLevel = 1; // Beyond lastLevelIndex
+            var provider = new CircularIndexAfterCompletingAllLevelsProvider(
+                firstLevelIndex, levelCount, circulateFromLevelIndex, currentLevel);
+
+            var testCircularIndexProvider =
+                new CircularIndexProvider(circulateFromLevelIndex, firstLevelIndex + levelCount,
+                    circulateFromLevelIndex);
+            for (int i = currentLevel; i < firstLevelIndex + 2 * levelCount; i++)
+            {
+                if (i <= firstLevelIndex + levelCount - 1)
+                    Assert.AreEqual(i, provider.Current);
+                else
+                {
+                    Assert.AreEqual(testCircularIndexProvider.Current, provider.Current);
+                    testCircularIndexProvider.MoveNext();
+                }
+
+                provider.MoveNext();
+            }
+        }
+    }
+}
