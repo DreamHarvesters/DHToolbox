a58dce98c7451709ed23aea08ca2eb571bfc71ad
Emre SOLAK <s.emresolak@dreamharvesters.com>
2023-11-11 22:34:00 +0300

    feat: object factory, persistency impl

---
+++ Runtime/DHToolboxAssembly/ObjectFactory/LeanPoolerFactory.cs
@@ -0,0 +1,57 @@
+using System;
+using Lean.Pool;
+using UnityEngine;
+
+namespace DHToolbox.Runtime.DHToolboxAssembly.WaveSystem
+{
+    public class LeanPoolerFactory : IObjectFactory
+    {
+        public GameObject Instantiate(GameObject prefab, Vector3 position, Quaternion rotation)
+        {
+#if LEAN_POOL
+            return LeanPool.Spawn(prefab, position, rotation);
+#endif
+            throw new Exception("Install LeanPool and add LEAN_POOL precompiler definition");
+        }
+
+        public GameObject Instantiate(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent)
+        {
+#if LEAN_POOL
+            return LeanPool.Spawn(prefab, position, rotation, parent);
+#endif
+            throw new Exception("Install LeanPool and add LEAN_POOL precompiler definition");
+        }
+
+        public GameObject Instantiate(GameObject prefab, Transform parent, bool worldPositionStays)
+        {
+#if LEAN_POOL
+            return LeanPool.Spawn(prefab, parent, worldPositionStays);
+#endif
+            throw new Exception("Install LeanPool and add LEAN_POOL precompiler definition");
+        }
+
+        public GameObject Instantiate(GameObject prefab, Transform parent)
+        {
+#if LEAN_POOL
+            return LeanPool.Spawn(prefab, parent);
+#endif
+            throw new Exception("Install LeanPool and add LEAN_POOL precompiler definition");
+        }
+
+        public GameObject Instantiate(GameObject prefab)
+        {
+#if LEAN_POOL
+            return LeanPool.Spawn(prefab);
+#endif
+            throw new Exception("Install LeanPool and add LEAN_POOL precompiler definition");
+        }
+
+        public void Destroy(GameObject gameObject, float delay)
+        {
+#if LEAN_POOL
+            LeanPool.Despawn(gameObject, delay);
+#endif
+            throw new Exception("Install LeanPool and add LEAN_POOL precompiler definition");
+        }
+    }
+}
--- Runtime/DHToolboxAssembly/Persistency/IPersistency.cs
+++ Runtime/DHToolboxAssembly/Persistency/IPersistency.cs
@@ -7,5 +7,17 @@ namespace DHToolbox.Runtime.DHToolboxAssembly.Persistency
         void SetFloat(string key, float value);
 
         void SetString(string key, string value);
+
+        void DeleteKey(string key);
+
+        void DeleteAll();
+
+        void GetInt(string key, int defaultValue);
+        void GetFloat(string key, float defaultValue);
+        void GetString(string key, string defaultValue);
+
+        bool HasKey(string key);
+
+        void Save();
     }
 }
